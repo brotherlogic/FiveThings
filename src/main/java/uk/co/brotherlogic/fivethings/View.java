4d6e7db81f933fbb2ea22344b9d07f2986d0657f
Simon <s.tucker@dcs.shef.ac.uk>
2010-05-03 10:37:55 +0100

    Added the view

---
+++ src/main/java/uk/co/brotherlogic/fivethings/View.java
@@ -0,0 +1,16 @@
+package uk.co.brotherlogic.fivethings;
+
+import javax.swing.JFrame;
+
+/**
+ * The main view for the five things app
+ * @author simon
+ *
+ */
+public class View extends JFrame
+{
+	public View()
+	{
+		
+	}
+}
