ebb836dea909822c3f43b30febce1874d5226400
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-05-03 14:51:49 +0100

    Updated to add maven exec

---
+++ src/main/java/uk/co/brotherlogic/fivethings/storage/IBackendStorage.java
@@ -0,0 +1,30 @@
+package uk.co.brotherlogic.fivethings.storage;
+
+import java.util.Collection;
+
+import uk.co.brotherlogic.fivethings.ToDo;
+
+/**
+ * To deal with the backend storage
+ * 
+ * @author simon
+ * 
+ */
+public interface IBackendStorage
+{
+	/**
+	 * Retrieves the days todos from the given storage module
+	 * 
+	 * @return A collection of todos that are appropriate for the day
+	 */
+	Collection<ToDo> getTodaysTodos();
+
+	/**
+	 * Stores the todo
+	 * 
+	 * @param toStore
+	 *            The todo to store
+	 * @return whether the todo was added
+	 */
+	boolean storeToDo(ToDo toStore);
+}
